e6b8a99476dc8bc9217ec137da3b6a8cac1faaaf
shashank.kadge <shashank_kadge@hotmail.com>
2025-08-30 15:16:40 -0400

    person added

---
+++ WorkerService/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace WorkerService
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public string GetName(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    Name = "me";
+                    break;
+                case 1:
+                    Name = "you";
+                    break;
+                default:
+                    break;
+            }
+            return Name;
+        }
+    }
+}
